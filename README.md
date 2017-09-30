59820e7cbdc2949a5c5f46b78a0a8988660853be
TheBartle <bartekx2014@gmail.com>
2017-09-30 18:50:07 +0200

    Cookie Clicker

---
+++ README.md
@@ -0,0 +1,14 @@
+# Clicker Project
+
+## UPDATES
+
+> LAST UPDATE ***DATE:*** **29.09.2017** | ***TIME:*** **20:10**
+
+> LAST UPDATE ***DATE:*** **30.09.2017** | ***TIME:*** **15:20**
+
+## To do:
+- [x] Add money by clicking on the cookie.
+- [x] Add shop (Money per second)
+- [ ] Add advanced shop
+- [ ] Add saving progress.
+- [ ] Add history (For example: You're a baker. Your goal is...)
---
+++ game.js
@@ -0,0 +1,64 @@
+$('img.cookie').on('click', function() {
+        $('.cookie').toggleClass('active');
+    });
+
+var Money = 0;
+var Bakers = 0;
+var BakersCost = 15;
+
+var Grandma = 0;
+var GrandmaCost = 100;
+
+function addMoney(number) {
+    Money = Money + number;
+    document.getElementById("moneyCount").innerHTML = Money;
+}
+function BuyBakers() {
+    if (Money >= BakersCost) {
+        Money = Money - BakersCost;
+        BakersCost = BakersCost + 5;
+        Bakers = Bakers + 1;
+        document.getElementById("Bakers").innerHTML = Bakers
+        document.getElementById("BakersCost").innerHTML = BakersCost
+        document.getElementById("moneyCount").innerHTML = Money
+    }
+}
+function BuyGrandma() {
+    if (Money >= GrandmaCost) {
+        Money = Money - GrandmaCost;
+        GrandmaCost = GrandmaCost + 25;
+        Grandma = Grandma + 1;
+        document.getElementById("Grandma").innerHTML = Grandma
+        document.getElementById("GrandmaCost").innerHTML = GrandmaCost
+        document.getElementById("moneyCount").innerHTML = Money
+    }
+}
+//Grandmas
+window.setInterval(
+    function(){
+    addMoney(Grandma);
+    addMoney(Grandma);
+    addMoney(Grandma);
+    addMoney(Grandma);
+    addMoney(Grandma);
+    addMoney(Grandma);
+    addMoney(Grandma);
+}, 975);
+//Bakers
+window.setInterval(
+    function(){
+    addMoney(Bakers);
+}, 1000);
+
+//SHOP
+function shopOpen() {
+    document.getElementById("shop").style.display = "block";
+    document.getElementById("HamO").style.display = "none";
+    document.getElementById("HamC").style.display = "block";
+}
+function shopClose() {
+    document.getElementById("shop").style.display = "none";
+    document.getElementById("shop").style.position = "relative";
+    document.getElementById("HamC").style.display = "none";
+    document.getElementById("HamO").style.display = "block";
+}
