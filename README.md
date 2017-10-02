7e78032f723555c5ac4527930daa659f08082d08
TheBartle <bartekx2014@gmail.com>
2017-10-02 20:04:37 +0200

    Cookie Clicker

--- README.md
+++ README.md
@@ -8,16 +8,15 @@
 
 > LAST UPDATE ***DATE:*** **30.09.2017** | ***TIME:*** **18:40**
 
+> LAST UPDATE ***DATE:*** **01.10.2017**  | ***TIME:*** **18:00**
+
+> LAST UPDATE ***DATE:*** **02.10.2017**  | ***TIME:*** **20:00**
+
 ## To do:
 - [x] Add money by clicking on the cookie.
 - [x] Add shop (Money per second).
 - [x] Add advanced shop.
 - [ ] Add saving progress.
-<<<<<<< HEAD
-- [ ] Add history (For example: You're a baker. Your goal is...)
-- [ ] Make game in 100%
-=======
 - [ ] Add goal.
-- [ ] Add upgrades.
+- [ ] Add upgrades. IN 50%
 - [ ] Add history (For example: You're a baker.).
->>>>>>> bad9df3395efcdfa518b85abfd0a19317d432cd9
--- game.js
+++ game.js
@@ -3,6 +3,8 @@ $('img.cookie').on('click', function() {
     });
 
 var Money = 0;
+var cor = new Audio('sound/Tink.mp3');
+var icor = new Audio('sound/Dong.mp3');
 
 var Bakers = 0;
 var BakersCost = 15;
@@ -12,19 +14,27 @@ var Grandma = 0;
 var GrandmaCost = 100;
 var GrandmaPower = 0;
 
+var BakersUpgrade = 0;
+var BakersUpgradeCost = 200;
+
 function addMoney(number) {
     Money = Money + number;
     document.getElementById("moneyCount").innerHTML = Money;
 }
 function BuyBakers() {
     if (Money >= BakersCost) {
-        Money = Money - BakersCost;
+        Money -= BakersCost;
         BakersCost = BakersCost + 5;
-        Bakers = Bakers + 1;
-        BakersPower = BakersPower = 1;
-        document.getElementById("Bakers").innerHTML = Bakers
-        document.getElementById("BakersCost").innerHTML = BakersCost
-        document.getElementById("moneyCount").innerHTML = Money
+        Bakers += 1;
+        Bakers * BakersPower;
+        document.getElementById("Bakers").innerHTML = Bakers;
+        document.getElementById("BakersCost").innerHTML = BakersCost;
+        document.getElementById("moneyCount").innerHTML = Money;
+            BakersPower += 1;
+        cor.play();
+    }
+    else {
+      icor.play();
     }
 }
 function BuyGrandma() {
@@ -32,10 +42,28 @@ function BuyGrandma() {
         Money = Money - GrandmaCost;
         GrandmaCost = GrandmaCost + 25;
         Grandma = Grandma + 1;
+        Grandma * GrandmaPower;
+        document.getElementById("Grandma").innerHTML = Grandma;
+        document.getElementById("GrandmaCost").innerHTML = GrandmaCost;
+        document.getElementById("moneyCount").innerHTML = Money;
         GrandmaPower = GrandmaPower = 5;
-        document.getElementById("Grandma").innerHTML = Grandma
-        document.getElementById("GrandmaCost").innerHTML = GrandmaCost
-        document.getElementById("moneyCount").innerHTML = Money
+        cor.play();
+    }
+    else {
+      icor.play();
+    }
+}
+function BuyBakersUpgrade() {
+    if (Money >= BakersUpgradeCost) {
+        Money = Money - BakersUpgradeCost;
+        BakersUpgradeCost = BakersUpgradeCost + 300;
+        BakersUpgrade = BakersUpgrade + 1;
+        BakersPower = BakersPower * 2;
+        document.getElementById("BakersUpgrade").innerHTML = BakersUpgrade;
+        document.getElementById("BakersUpgradeCost").innerHTML = BakersUpgradeCost;
+    }
+    else {
+      icor.play();
     }
 }
 //Grandmas
@@ -46,18 +74,17 @@ window.setInterval(
 //Bakers
 window.setInterval(
     function(){
-    addMoney(BakersPower);
+    addMoney(Bakers);
 }, 1000);
 
 //SHOP
 function shopOpen() {
     document.getElementById("shop").style.display = "block";
     document.getElementById("HamO").style.display = "none";
-    document.getElementById("HamC").style.display = "block";
+    document.getElementById("HamC").style.display = "table";
 }
 function shopClose() {
     document.getElementById("shop").style.display = "none";
-    document.getElementById("shop").style.position = "relative";
     document.getElementById("HamC").style.display = "none";
-    document.getElementById("HamO").style.display = "block";
+    document.getElementById("HamO").style.display = "table";
 }
